79cfde651ef298c7c2d5aa4fc5b3d2afb2e346cb
iwantmyhatback <iwantmyhatbackk@gmail.com>
2025-05-17 20:50:43 -0400

    Cleanup

--- couched/python/braviatv.py
+++ couched/python/braviatv.py
@@ -35,10 +35,12 @@ default_hdmi_title = 'HDMI 3/ARC'
 default_hdmi_command = 'Hdmi3'
 
 braviarc = BraviaRC(ip_address)
+# Implement monkey-patch
 braviarc.set_volume_level = types.MethodType(patched_set_volume_level, braviarc)
 
-# Connect to TV
+# Main Execution
 def main():
+    # Connect to device
     log.info(f'[PYTHON] Connecting to {ip_address}')
     braviarc.connect(pin, 'fooby', 'fooby')
     if not braviarc.is_connected():
@@ -54,7 +56,7 @@ def main():
     if power_status != 'active':
         raise RuntimeError('[ERROR] Power status hasnt changed to active after get_power_status()')
 
-    # Normalize the volume
+    # Normalize the volume level
     log.info(f'[PYTHON] Normalizing volume 🔉')
     volume_info = braviarc.get_volume_info()
     if not volume_info:
@@ -76,41 +78,48 @@ def main():
         log.info(f'[PYTHON] Volume is: {current_volume} thats just right 😮‍💨')
 
 
-
-    # Check theinput
+    # Normalize the input source
     source_list = {}
     log.info('[PYTHON] Normalizing input source 📺')
     log.info(f'[PYTHON] Setting input source: "{default_hdmi_command}"')
-    
-    # IR Command is faster and more reliable
+
+    # Change input source to default
+    braviarc.send_command(default_hdmi_command)
+    # [REMOVED] IR Command is faster and more reliable
     # braviarc.select_source(default_source)
     
-    braviarc.send_command(default_hdmi_command)
+    # Validate the input source change
     time.sleep(1) # get_playing_info() has a little lag before it populates
     playing_info = braviarc.get_playing_info()
- 
     if playing_info.get('title') != default_hdmi_title:
         log.warning(f'[PYTHON] Source selction issue detected: {playing_info.get('title')} != {default_hdmi_title}')
         source_list = braviarc.load_source_list()
+
+        # Give it one more try if the device shows in the source list (if its not there throw an error)
         if source_list.get(default_hdmi_title):
             log.info(f'[PYTHON] Found Source \"{default_hdmi_title}\" in source_list after failed select_source()')
             log.info(f'[PYTHON] Retrying source selection to "{default_hdmi_title}"')
-            # IR Command is faster and more reliable
-            # braviarc.select_source(default_source)
             braviarc.send_command(default_hdmi_command)
+            # [REMOVED] IR Command is faster and more reliable
+            # braviarc.select_source(default_source)
         else:
             raise RuntimeError(f'[ERROR] Source: \"{default_hdmi_title}\" doesnt appear to be connected according to load_source_list()')
     
+    # Throw exception if it still hasnt changed input sources
     time.sleep(1)
     playing_info = braviarc.get_playing_info()
-
     if playing_info.get('title') != default_hdmi_title:
         raise RuntimeError(f'[ERROR] Source didnt change to \"{default_hdmi_title}\" after running select_source()')
     
+    # Make sure were in the AndroidTV menu
+    # (In case we resume a recent session and it drops us somewhere non standard)
+    braviarc.send_command('Return')
+    braviarc.send_command('Return')
     braviarc.send_command('Return')
     braviarc.send_command('Return')
     braviarc.send_command('Return')
 
+    # Make sure the cursor is in the top-left corner of AndroidTV
     bruteforce = 10
     while bruteforce > 0:
         braviarc.send_command('CursorUp')
@@ -119,7 +128,7 @@ def main():
         time.sleep(.01)
         bruteforce -= 1
 
-    # Tailscale Start
+    # Navigate to "Apps" tab in the top menu. (Make sure tab is selected with a few CursorUp's)
     braviarc.send_command('CursorRight')
     braviarc.send_command('CursorRight')
     braviarc.send_command('CursorRight')
@@ -130,21 +139,27 @@ def main():
     braviarc.send_command('CursorUp')
     braviarc.send_command('CursorUp')
     braviarc.send_command('CursorUp')
+    
+    # Navigate to "Tailscale", click it, and Wait for the app to start
     braviarc.send_command('CursorDown')
     braviarc.send_command('CursorDown')
     braviarc.send_command('CursorRight')
     braviarc.send_command('DpadCenter')
     time.sleep(7.5)
+
+    # Return to "Applications" page
     braviarc.send_command('Return')
     time.sleep(2)
 
-    # Jellyfin Start
+    # Navigate to "Jellyfin" and click it
     braviarc.send_command('CursorLeft')
     braviarc.send_command('DpadCenter')
 
-    
+    log.info('[PYTHON] Done! 🎉 🎉 🎉 🎉 🎉')
 
-
+# I was having issues with getting the Search to behave consistently
+# Kept making me do licensing stuff after a power cycle.
+# I prefer this over location based selection, but it is too unreliable
 ## SEARCH LOGIC START
     # braviarc.send_command('CursorRight')
     # braviarc.send_command('DpadCenter')
@@ -216,27 +231,3 @@ def main():
     # braviarc.send_command('CursorLeft')
     # braviarc.send_command('CursorRight')
 ## SEARCH LOGIC END
-
-    # print(json.dumps(source_list, indent=2))
-    # print(json.dumps(playing_info, indent=2))
-    # braviarc.set_volume_level(0)
-
-
-
-    # print('applist::')
-    # resp = braviarc.load_app_list()
-    # print(resp)
-
-
-
-    # # Get playing info
-    # playing_content = braviarc.get_playing_info()
-
-    # # Print current playing channel
-    # print (playing_content.get('title'))
-
-    # # Get volume info
-    # volume_info = braviarc.get_volume_info()
-
-    # # Print current volume
-    # print (volume_info.get('volume'))
